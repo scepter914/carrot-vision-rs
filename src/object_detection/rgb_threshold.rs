694c5d6043265fa368bcf8bad713a597bd42cd59
scepter914 <st14.828soccer@gmail.com>
2021-08-01 03:36:01 +0900

    done binalization

--- src/object_detection/rgb_threshold.rs
+++ src/object_detection/rgb_threshold.rs
@@ -32,27 +32,47 @@ pub fn binalize_rgb(image: &RgbImage, rgb_threshold: &RGBThreshold) -> GrayImage
 }
 
 pub fn is_within_threshold(pixel: &image::Rgb<u8>, rgb_threshold: &RGBThreshold) -> bool {
-  return is_within(pixel, rgb_threshold, true);
-}
-
-pub fn is_withinout_threshold(pixel: &image::Rgb<u8>, rgb_threshold: &RGBThreshold) -> bool {
-  return is_within(pixel, rgb_threshold, false);
-}
-
-/* is_within
- If is_within is true, pixel between low_threshold and high_threshold binarize to 1
- If is_within is false, pixel between low_threshold and high_threshold binarize to 0
-*/
-fn is_within(pixel: &image::Rgb<u8>, rgb_threshold: &RGBThreshold, is_within: bool) -> bool {
-  if rgb_threshold.r_low < pixel[0]
-    && pixel[0] < rgb_threshold.r_high
-    && rgb_threshold.g_low < pixel[1]
-    && pixel[1] < rgb_threshold.g_high
-    && rgb_threshold.b_low < pixel[2]
-    && pixel[2] < rgb_threshold.b_high
+  if rgb_threshold.r_low <= pixel[0]
+    && pixel[0] <= rgb_threshold.r_high
+    && rgb_threshold.g_low <= pixel[1]
+    && pixel[1] <= rgb_threshold.g_high
+    && rgb_threshold.b_low <= pixel[2]
+    && pixel[2] <= rgb_threshold.b_high
   {
-    return is_within;
+    return true;
   } else {
-    return !is_within;
+    return false;
   }
+}
+
+pub fn is_without_threshold(pixel: &image::Rgb<u8>, rgb_threshold: &RGBThreshold) -> bool {
+  if is_within_threshold(pixel, rgb_threshold) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+pub fn disassemble_rgb(image: &RgbImage) -> (image::GrayImage, image::GrayImage, image::GrayImage) {
+  let width = image.width();
+  let height = image.height();
+
+  // init
+  let mut r_image = image::GrayImage::new(width, height);
+  let mut g_image = image::GrayImage::new(width, height);
+  let mut b_image = image::GrayImage::new(width, height);
+
+  for i in 0..width {
+    for j in 0..height {
+      let pixel = image.get_pixel(i, j);
+      r_image.put_pixel(i, j, image::Luma([pixel[0]; 1]));
+      g_image.put_pixel(i, j, image::Luma([pixel[1]; 1]));
+      b_image.put_pixel(i, j, image::Luma([pixel[2]; 1]));
+    }
+  }
+  return (r_image, g_image, b_image);
+}
+
+pub fn print_pixel(pixel: &image::Rgb<u8>) -> () {
+  println!("{}, {}, {}", pixel[0], pixel[1], pixel[2]);
 }
--- src/riprs_cv_lib/file.rs
+++ src/riprs_cv_lib/file.rs
@@ -1,3 +1,6 @@
+// extern crate chrono;
+// use chrono::{Date, DateTime, Local, Utc};
+
 pub struct FilePath {
   directory_path: String,
   file_path: String,
@@ -10,6 +13,14 @@ impl FilePath {
       file_path: file_path.into(),
     }
   }
+
+  //pub fn new_from_time(directory_path: impl Into<String>) -> FilePath {
+  //  file_path =
+  //  FilePath {
+  //    directory_path: directory_path.into(),
+  //    file_path:
+  //  }
+  //}
 
   pub fn get_file_path(&self) -> String {
     return format!("{}{}", self.directory_path, self.file_path);
