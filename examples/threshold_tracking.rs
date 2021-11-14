9eb5708df81011c41b34972859c00c3bcd0fc67b
scepter914 <scepter914@gmail.com>
2021-11-14 21:03:15 +0900

    devide crate
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- examples/threshold_tracking.rs
+++ examples/threshold_tracking.rs
@@ -1,14 +1,19 @@
 use image::imageops;
 use image::RgbImage;
 use imageproc::point::Point;
+use log::LevelFilter;
 use log::{log_enabled, Level};
 use std::env;
+use std::path::PathBuf;
 
 use realtime_cv::object_detection::threshold_detection;
 use realtime_cv_lib::convertor::rgb_to_binary;
-use realtime_cv_lib::{debug, image_util, logger};
+use realtime_cv_lib::{debug, image_util};
 
-fn threshold_detection(input_image: &RgbImage, logger: &logger::Logger) {
+use carrot_util_library::file;
+use carrot_util_library::logger::CarrotLogger;
+
+fn threshold_detection(input_image: &RgbImage, log_directory_path: PathBuf) {
     let rgb_threshold = rgb_to_binary::RGBThreshold {
         high_threshold: image::Rgb([255, 255, 80]),
         low_threshold: image::Rgb([100, 0, 0]),
@@ -33,41 +38,51 @@ fn threshold_detection(input_image: &RgbImage, logger: &logger::Logger) {
     // If log level is Debug or Trace, then save the image of center gravity debug image
     if log_enabled!(Level::Debug) {
         let cg_image = threshold_detection::get_cg_debug_image(&input_image, &cg);
-        // cg_image
-        //     .save(logger.get_time_path("threshold_detection_", "png"))
-        //     .unwrap();
-        image_util::save_ppm_file(
-            &cg_image,
-            std::path::Path::new(&logger.get_time_path("threshold_detection_", "ppm")),
-        )
-        .unwrap();
+        let file_path =
+            log_directory_path.join(file::get_time_filepath("threshold_detection_", "ppm"));
+        image_util::save_ppm_file(&cg_image, file_path.as_path()).unwrap();
     }
 
     // If log level is Trace, then save the image of threshold rgb debug image
     if log_enabled!(Level::Trace) {
         let rgb_disassembled_image =
             rgb_to_binary::get_rgb_threshold_debug_image(&input_image, &rgb_threshold);
+        let rgb_disassembled_image_path = log_directory_path.join("result_rgb_layer.png");
         rgb_disassembled_image
-            .save(logger.get_full_path("result_rgb_layer.png"))
+            .save(rgb_disassembled_image_path)
             .unwrap();
     }
 }
 
+fn set_log_level(log_level: &str) -> LevelFilter {
+    let logger_level: LevelFilter;
+    match &log_level[..] {
+        "Error" => logger_level = LevelFilter::Error,
+        "Warn" => logger_level = LevelFilter::Warn,
+        "Info" => logger_level = LevelFilter::Info,
+        "Debug" => logger_level = LevelFilter::Debug,
+        "Trace" => logger_level = LevelFilter::Trace,
+        _ => logger_level = LevelFilter::Error,
+    }
+    logger_level
+}
+
 fn main() {
-    let log_level;
+    let log_level_input;
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        log_level = "Error";
+        log_level_input = "Error";
     } else {
-        log_level = &args[1];
+        log_level_input = &args[1];
     }
 
-    let logger = logger::Logger::init(log_level, "./data/result");
+    let logger_level = set_log_level(log_level_input);
+    let logger = CarrotLogger::init("./data/result", logger_level, logger_level);
 
     let input_image_path = "./data/ball_2.jpg";
     let input_image_ = image::open(input_image_path).unwrap().to_rgb8();
     let input_image = imageops::resize(&input_image_, 640, 360, imageops::Gaussian);
     debug::print_image_info(&input_image);
 
-    threshold_detection(&input_image, &logger);
+    threshold_detection(&input_image, logger.root_result_directory);
 }
