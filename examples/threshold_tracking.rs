4cab9f333486803777e9f754f4a6947e41d46d68
scepter914 <scepter914@gmail.com>
2022-05-07 16:55:32 +0900

    update examples with carrot_image_util
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- examples/threshold_tracking.rs
+++ examples/threshold_tracking.rs
@@ -1,57 +1,28 @@
 use image::imageops;
-use image::RgbImage;
+use image::{GrayImage, RgbImage};
 use imageproc::point::Point;
+use log::debug;
 use log::LevelFilter;
 use log::{log_enabled, Level};
 use std::env;
-use std::path::PathBuf;
+
+use carrot_cv_utils::image::convertor::rgb_to_binary;
+use carrot_cv_utils::image::rgb_image::debug;
+use carrot_cv_utils::image::rgb_image::file;
+use carrot_cv_utils::util::benchmark::Benchmark;
+use carrot_cv_utils::util::logger::Logger;
+use carrot_cv_utils::util::path;
 
 use realtime_cv::object_detection::threshold_detection;
-use realtime_cv_lib::convertor::rgb_to_binary;
-use realtime_cv_lib::{debug, image_util};
 
-use carrot_util_library::file;
-use carrot_util_library::logger::CarrotLogger;
-
-fn threshold_detection(input_image: &RgbImage, log_directory_path: PathBuf) {
-    let rgb_threshold = rgb_to_binary::RGBThreshold {
-        high_threshold: image::Rgb([255, 255, 80]),
-        low_threshold: image::Rgb([100, 0, 0]),
-    };
-
-    let binarized_image: image::GrayImage;
-    let cg: Point<f32>;
-    if log_enabled!(Level::Info) {
-        let benchmark = debug::Benchmark::set_start_time();
-        binarized_image = rgb_to_binary::convert_by_threshold(&input_image, &rgb_threshold);
-        cg = threshold_detection::get_cg_from_binary(&binarized_image);
-        benchmark.print_bench_time();
-        debug::print_point_info("cg", &cg);
-        if log_enabled!(Level::Debug) {
-            debug::print_pixel_from_point("cg", &input_image, cg);
-        }
-    } else {
-        binarized_image = rgb_to_binary::convert_by_threshold(&input_image, &rgb_threshold);
-        cg = threshold_detection::get_cg_from_binary(&binarized_image);
-    }
-
-    // If log level is Debug or Trace, then save the image of center gravity debug image
-    if log_enabled!(Level::Debug) {
-        let cg_image = threshold_detection::get_cg_debug_image(&input_image, &cg);
-        let file_path =
-            log_directory_path.join(file::get_time_filepath("threshold_detection_", "ppm"));
-        image_util::save_ppm_file(&cg_image, file_path.as_path()).unwrap();
-    }
-
-    // If log level is Trace, then save the image of threshold rgb debug image
-    if log_enabled!(Level::Trace) {
-        let rgb_disassembled_image =
-            rgb_to_binary::get_rgb_threshold_debug_image(&input_image, &rgb_threshold);
-        let rgb_disassembled_image_path = log_directory_path.join("result_rgb_layer.png");
-        rgb_disassembled_image
-            .save(rgb_disassembled_image_path)
-            .unwrap();
-    }
+fn threshold_detection(
+    input_image: &RgbImage,
+    rgb_threshold: &rgb_to_binary::RGBThreshold,
+) -> (GrayImage, Point<f32>) {
+    let binarized_image: GrayImage =
+        rgb_to_binary::convert_by_threshold(input_image, rgb_threshold);
+    let cg: Point<f32> = threshold_detection::get_cg_from_binary(&binarized_image);
+    (binarized_image, cg)
 }
 
 fn set_log_level(log_level: &str) -> LevelFilter {
@@ -68,21 +39,58 @@ fn set_log_level(log_level: &str) -> LevelFilter {
 }
 
 fn main() {
-    let log_level_input;
+    // Set logger and benchmark
     let args: Vec<String> = env::args().collect();
+    let log_level_input: &str;
     if args.len() < 2 {
         log_level_input = "Error";
     } else {
         log_level_input = &args[1];
     }
-
     let logger_level = set_log_level(log_level_input);
-    let logger = CarrotLogger::init("./data/result", logger_level, logger_level);
+    let logger = Logger::init("./data/result", logger_level, logger_level);
+    let mut benchmark: Benchmark = Benchmark::new();
 
-    let input_image_path = "./data/ball_2.jpg";
+    // Set input
+    let input_image_path = "./data/ball.jpg";
     let input_image_ = image::open(input_image_path).unwrap().to_rgb8();
     let input_image = imageops::resize(&input_image_, 640, 360, imageops::Gaussian);
-    debug::print_image_info(&input_image);
 
-    threshold_detection(&input_image, logger.root_result_directory);
+    // Threshold detection
+    let rgb_threshold = rgb_to_binary::RGBThreshold {
+        high_threshold: image::Rgb([255, 255, 80]),
+        low_threshold: image::Rgb([100, 0, 0]),
+    };
+    if log_enabled!(Level::Debug) {
+        benchmark.start();
+    }
+
+    let binarized_image: GrayImage;
+    let cg: Point<f32>;
+    (binarized_image, cg) = threshold_detection(&input_image, &rgb_threshold);
+    debug!("{}", benchmark.get_bench_time());
+    debug!(
+        "cg: {:?}, {}",
+        cg,
+       debug::print_pixel_from_point(&input_image, cg),
+    );
+
+    // Debug image
+    if log_enabled!(Level::Debug) {
+        let cg_image = threshold_detection::get_cg_debug_image(&input_image, &cg);
+        let file_path = logger
+            .root_result_directory
+            .join(path::get_time_filepath("threshold_detection_", "ppm"));
+        file::save_ppm_file(&cg_image, file_path.as_path()).unwrap();
+    }
+
+    // If log level is Trace, then save the image of threshold rgb debug image
+    if log_enabled!(Level::Trace) {
+        let rgb_disassembled_image =
+            rgb_to_binary::get_rgb_threshold_debug_image(&input_image, &rgb_threshold);
+        let rgb_disassembled_image_path = logger.root_result_directory.join("result_rgb_layer.png");
+        rgb_disassembled_image
+            .save(rgb_disassembled_image_path)
+            .unwrap();
+    }
 }
