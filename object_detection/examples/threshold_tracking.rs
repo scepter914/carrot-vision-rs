7dfd6e91812a2160a77035e56b9d5e227367a924
scepter914 <scepter914@gmail.com>
2022-08-15 01:27:49 +0900

    fix crate path
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- object_detection/examples/threshold_tracking.rs
+++ object_detection/examples/threshold_tracking.rs
@@ -1,20 +1,23 @@
 use image::imageops;
-use image::{GrayImage, RgbImage};
+use image::GrayImage;
+use imageproc::drawing;
 use imageproc::point::Point;
+use imageproc::rect::Rect;
+
 use log::debug;
 use log::LevelFilter;
 use log::{log_enabled, Level};
 use std::env;
 
-use carrot_cv_utils::image::convertor::rgb_to_binary;
-use carrot_cv_utils::image::rgb_image::debug;
-use carrot_cv_utils::image::rgb_image::draw;
-use carrot_cv_utils::image::rgb_image::file;
-use carrot_cv_utils::util::benchmark::Benchmark;
-use carrot_cv_utils::util::logger::Logger;
-use carrot_cv_utils::util::path;
+use carrot_image_utils::geometry::rect;
+use carrot_image_utils::rgb_image::draw;
+use carrot_image_utils::rgb_image::file;
+use carrot_image_utils::rgb_image::rgb_to_binary;
+use carrot_utils::benchmark::Benchmark;
+use carrot_utils::logger::Logger;
+use carrot_utils::path::format_time_macro;
 
-use realtime_cv::object_detection::threshold_detection;
+use object_detection::object_detection::threshold_detection;
 
 fn set_log_level(log_level: &str) -> LevelFilter {
     let logger_level: LevelFilter;
@@ -39,7 +42,12 @@ fn main() {
         log_level_input = &args[1];
     }
     let logger_level = set_log_level(log_level_input);
-    let logger = Logger::init("./data/result", logger_level, logger_level);
+    let logger = Logger::new(
+        "./data/result/{TIME_SEC}",
+        "{TIME_SEC}.txt",
+        logger_level,
+        logger_level,
+    );
     let mut benchmark: Benchmark = Benchmark::new();
 
     // Set input
@@ -60,20 +68,17 @@ fn main() {
         rgb_to_binary::convert_by_threshold(&input_image, &rgb_threshold);
     let cg: Point<f32> = threshold_detection::get_cg_from_binary(&binarized_image);
 
-    debug!("{}", benchmark.get_bench_time());
-    debug!(
-        "cg: {:?}, {}",
-        cg,
-        debug::get_pixel_string(&input_image, cg)
-    );
+    debug!("{}", benchmark.stop());
 
     // Debug image
     if log_enabled!(Level::Debug) {
         let mut cg_image = draw::draw_point(&input_image, cg, image::Rgb([0, 0, 255]));
-        cg_image = draw::draw_rect(&cg_image, cg, 480, 480, image::Rgb([0, 0, 255]));
-        let file_path = logger
-            .root_result_directory
-            .join(path::get_time_filepath("threshold_detection_", "ppm"));
+
+        let rect_: Rect = rect::make_rect_from_chw(cg, 480, 480);
+        cg_image = drawing::draw_hollow_rect(&cg_image, rect_, image::Rgb([0, 0, 255]));
+        let file_path = logger.log_directory_path.join(format_time_macro(
+            "threshold_detection_{TIME_MS}.ppm".to_string(),
+        ));
         file::save_ppm_file(&cg_image, file_path.as_path()).unwrap();
     }
 
@@ -81,7 +86,7 @@ fn main() {
     if log_enabled!(Level::Trace) {
         let rgb_disassembled_image =
             rgb_to_binary::get_rgb_threshold_debug_image(&input_image, &rgb_threshold);
-        let rgb_disassembled_image_path = logger.root_result_directory.join("result_rgb_layer.png");
+        let rgb_disassembled_image_path = logger.log_directory_path.join("result_rgb_layer.png");
         rgb_disassembled_image
             .save(rgb_disassembled_image_path)
             .unwrap();
